c5ff2d05414fa8960f49edfcf2d80ca5ce00b701
VieuxLoupGris <provocemois@gmail.com>
2019-01-29 12:15:18 +0100

    Fonctionnal program who ask the user to find the guess number

---
+++ Exercise_7.1.js
@@ -0,0 +1,32 @@
+ /*
+  **Make a program that generates an integer between 1 and 100 (don't display it to the user).
+  **Then the program should display "Guess the number" and ask the user to guess.
+  **If the user enters a number which is too low it should display "Too low" and re-ask the question.
+  **If the user enters a number which is too high it should display "Too high" and re-ask the question.
+  **If the user guess correctly it should display "Well guessed!" and exit.
+  */
+
+ const readLineSync = require("readline-sync"); //Call the module for ask the user.
+
+
+ let guessNumber = () => {
+
+   let result = Math.floor(Math.random() * 100);
+   let userResult
+   do {
+     userResult = readLineSync.question("Guess The number: ");
+     if (result < userResult) {
+       console.log("To hight");
+
+     } else if (result > userResult) {
+       console.log("To low");
+
+     } else {
+       console.log("You win");
+     }
+
+   } while (userResult != result);
+
+ }
+
+ guessNumber();
